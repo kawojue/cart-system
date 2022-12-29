1744b492c8ec6dcebe8b9ed61960f1ad16171207
Raheem Kawojue <kawojue08@gmail.com>
2022-12-29 15:51:03 +0100

    [add] Data Provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import './index.css'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { DataProvider } from './components/Context'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </React.StrictMode>,
 )
