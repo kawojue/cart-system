13bd0037b5c299cb7e460980e7fff0ec8d068995
Raheem Kawojue <kawojue08@gmail.com>
2022-12-29 14:34:30 +0100

    [add] font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cabin: ['Cabin', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
