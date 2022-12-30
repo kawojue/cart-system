2ccf678a64b2c8173ac5d1a4f308552deeff624c
Raheem Kawojue <kawojue08@gmail.com>
2022-12-30 21:38:41 +0100

    [edit] incre & decre btn

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,29 +1,32 @@
-import React from "react";
-import { ICarts } from "../interface";
+import Context from './Context'
+import { ICarts } from "../IType"
+import React, { useContext } from "react"
 import { FaChevronUp } from 'react-icons/fa'
 import { FaChevronDown } from 'react-icons/fa'
 
 const Item: React.FC<ICarts> = ({id, title, price, img, amount}) => {
+    const { quantity } = useContext<any>(Context)
+
     return (
-            <article className="item-center">
-                <img src={img} className="img" />
-                <div className="item-info">
-                    <h2>{title}</h2>
-                    <p>${price}</p>
-                    <button>
-                        remove
-                    </button>
-                </div>
-                <div className="quantity-container">
-                    <button>
-                        <FaChevronUp />
-                    </button>
-                    <p>{amount}</p>
-                    <button>
-                        <FaChevronDown />
-                    </button>
-                </div>
-            </article>
+        <article className="item-center">
+            <img src={img} className="img" />
+            <div className="item-info">
+                <h2>{title}</h2>
+                <p>${price}</p>
+                <button>
+                    remove
+                </button>
+            </div>
+            <div className="quantity-container">
+                <button onClick={() => quantity(id, 'increment')}>
+                    <FaChevronUp />
+                </button>
+                <p>{amount}</p>
+                <button onClick={() => quantity(id, 'decrement')}>
+                    <FaChevronDown />
+                </button>
+            </div>
+        </article>
     )
 }
 
