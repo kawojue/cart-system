d1bfca2dff738df0e2f7eb3590c4100620c506f8
Raheem Kawojue <kawojue08@gmail.com>
2022-12-30 16:08:08 +0100

    [add] components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from './components/Header'
 import ItemLists from './components/ItemLists'
+import TotalAmount from './components/TotalAmout'
 
 const App: React.FC = () => {
   return (
     <>
       <Header totalItem={0} />
-      <main>
+      <main className="main">
+        <h1 className="main-h1 md:text-5xl">your bag</h1>
         <ItemLists />
+        <TotalAmount />
       </main>
     </>
   )
