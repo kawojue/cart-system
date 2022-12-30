4ab67820b3c8ba615f4a1ddcbdefbf498e3cf33e
Raheem Kawojue <kawojue08@gmail.com>
2022-12-30 22:16:44 +0100

    [edit] totoal Item

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { FaShoppingBag } from 'react-icons/fa'
 
-interface IProps {
-    totalItem: number
-}
-
-const Header: React.FC<IProps> = ({totalItem}) => {
+const Header: React.FC<{totalItem: number}> = ({totalItem}) => {
     return (
         <header className="header md:text-4xl">
             <nav className="navbar md:px-20">
