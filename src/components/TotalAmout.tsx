37b13c52fdee7a21f8b39cb675326e140aa3df0c
Raheem Kawojue <kawojue08@gmail.com>
2022-12-31 01:31:15 +0100

    [add] total

--- src/components/TotalAmout.tsx
+++ src/components/TotalAmout.tsx
@@ -2,14 +2,14 @@ import Context from './Context'
 import React, { useContext } from 'react'
 
 const TotalAmount:React.FC = () => {
-    const { clearCart } = useContext<any>(Context)
+    const { clearCart, total } = useContext<any>(Context)
 
     return (
         <section className="item-total">
             <div className="line"/>
             <article>
                 <p>total</p>
-                <p>$54556.57</p>
+                <p>${total}</p>
             </article>
             <div className="btn-conatainer">
             <button onClick={() => clearCart()}>
