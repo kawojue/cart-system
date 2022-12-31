d9412772694a6476514194945ab6ab0f18024506
Raheem Kawojue <kawojue08@gmail.com>
2022-12-31 02:15:45 +0100

    [edit]

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { FaShoppingBag } from 'react-icons/fa'
 
-const Header: React.FC<{totalItem: number}> = ({totalItem}) => {
+const Header: React.FC<{ totalItem: number }> = ({ totalItem }) => {
     return (
         <header className="header md:text-4xl">
             <nav className="navbar md:px-20">
                 <h3>Cart System</h3>
                 <p className="bag">
                     <FaShoppingBag />
-                    <span className="totalItem">{totalItem}</span>
+                    <span className="totalItem">
+                        {totalItem}
+                    </span>
                 </p>
             </nav>
         </header>
