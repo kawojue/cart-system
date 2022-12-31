ff4cd46840157b7442d9b5388726a4c76259a0f7
Raheem Kawojue <kawojue08@gmail.com>
2022-12-31 02:16:07 +0100

    [fix] quantity func

--- src/components/Context.tsx
+++ src/components/Context.tsx
@@ -1,62 +1,54 @@
 import axios from 'axios'
-import { ICarts, State } from '../IType'
-import data from '../data'
 import { cartReducer } from '../cartReducer'
-import React, {createContext, useReducer, useEffect} from 'react'
+import { initialStates, IQuantityObj } from '../source'
+import React, { createContext, useReducer, useEffect } from 'react'
 
 const Context:any | null = createContext({})
 
-const initialStates:State = {
-    loading: true,
-    amount: 0,
-    total: 0,
-    carts: data,
-    errMsg: null
-}
-
-export const DataProvider: React.FC<{children: React.ReactElement}> = ({ children }) => {
+export const DataProvider: React.FC<{ children: React.ReactElement }> = ({ children }) => {
     const [state, dispatch] = useReducer(cartReducer, initialStates)
     const url:string = 'https://course-api.com/react-useReducer-cart-project'
 
     const fetchCarts = async ():Promise<void> => {
         try {
-            dispatch({type: 'ERROR', errMsg: null})
-            dispatch({type: 'LOADING', isLoading: true})
+            dispatch({ type: 'ERROR', errMsg: null })
+            dispatch({ type: 'LOADING', isLoading: true })
             const res = await axios.get(url)
             const data = res.data
-            dispatch({type: 'FETCH', payload: data})
+            dispatch({ type: 'FETCH', payload: data })
         } catch {
-            dispatch({type: 'ERROR', errMsg: 'Please, reload the page.'})
+            dispatch({ type: 'ERROR', errMsg: 'Please, reload the page.' })
         }
     }
 
     const removeCart = (id:string):void => {
-        dispatch({type: 'REM', idx: id})
+        dispatch({ type: 'REMOVE', idx: id })
     }
 
     const quantity = (id:string, action:string):void => {
         const getItem = state.carts.filter((cart:any) => cart.id === id)
-        const payload = {idx:id, quantity: getItem[0].amount}
+        const payload:IQuantityObj = { idx:id, quantity: getItem[0].amount }
+
         if (action === 'increment') {
-            dispatch({type: "INCREASE", quantObj: payload})
+            dispatch({ type: "INCREASE", quantObj: payload })
         }
 
         if (action === 'decrement') {
-            dispatch({type: "DECREASE", quantObj: payload})
-            if (getItem[0].amount === 0) {
-                dispatch({type: 'REM', idx: id})
+            if (getItem[0].amount <= 1) {
+                removeCart(id)
             }
+            dispatch({ type: "DECREASE", quantObj: payload })
         }
     }
 
     const clearCart = ():void => {
-        dispatch({type: 'CLEAR'})
+        dispatch({ type: 'CLEAR' })
     }
 
     useEffect(() => {
         (async () => await fetchCarts())()
         setTimeout(() => {
-            dispatch({type: 'LOADING', isLoading: false})
+            dispatch({ type: 'LOADING', isLoading: false })
         }, 1500)
     }, [])
 
@@ -64,12 +56,13 @@ export const DataProvider: React.FC<{children: React.ReactElement}> = ({ childre
         let total:number = 0
         const getTotal = state.carts.map((cart:any) => cart.amount * cart.price)
         getTotal.forEach((t:number) => total += t)
-        dispatch({type: 'TOTAL', total})
+        dispatch({ type: 'TOTAL', total })
     }, [state.carts])
 
     return (
         <Context.Provider value={{
-            ...state, quantity, removeCart, clearCart
+            ...state, quantity,
+            removeCart, clearCart
         }}>
             {children}
         </Context.Provider>
