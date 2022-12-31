d288b9f130b58382b5e13dc497dc754c98df044e
Raheem Kawojue <kawojue08@gmail.com>
2022-12-31 01:30:25 +0100

    [add] total amount state

--- src/cartReducer.ts
+++ src/cartReducer.ts
@@ -1,13 +1,5 @@
-import { State, ICarts, IQuantityObj} from "./IType"
+import { State, Actions} from "./IType"
 
-export type Actions = 
-    | {type: 'LOADING'; isLoading: boolean}
-    | {type: 'INCREASE'; quantObj: IQuantityObj}
-    | {type: 'DECREASE'; quantObj: IQuantityObj}
-    | {type: 'REM'; idx:string}
-    | {type: 'CLEAR'}
-    | {type: 'FETCH'; payload: ICarts[]}
-    | {type: 'ERROR'; errMsg: string | null}
 
 export const cartReducer = (state:State, action:Actions) => {
     switch(action.type) {
@@ -25,8 +17,19 @@ export const cartReducer = (state:State, action:Actions) => {
                 ...state, errMsg: action.errMsg
             }
         }
+        case 'TOTAL': {
+            return {
+                ...state, total: action.total
+            }
+        }
         case 'INCREASE': {
             const newCarts = state.carts.map((cart:any) => cart.id === action.quantObj.idx ? {...cart, amount: cart.amount += 1} : cart)
+            return {
+                ...state, carts: newCarts
+            }
+        }
+        case 'DECREASE': {
+            const newCarts = state.carts.map((cart:any) => cart.id === action.quantObj.idx ? {...cart, amount: cart.amount === 1 ? 0: cart.amount - 1} : cart)
             return {
                 ...state, carts: newCarts
             }
